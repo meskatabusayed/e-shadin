289f1e735714db171de6794d0f6f4e197c187493
meskatabusayed <meskatabusayed@gmail.com>
2024-08-15 15:03:49 +0530

    redux intialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='m-20'>
+        <h1>Value : 0</h1>
+        <button className='bg-blue-800 p-2 text-white font-bold rounded-md mt-2'>Add</button>
+        <button className='bg-blue-800 p-2 text-white font-bold ml-4 rounded-md mt-2'>Subtract</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
