9464886af541edba921d829c0afdaf0732eb57c3
meskatabusayed <meskatabusayed@gmail.com>
2024-08-15 20:58:21 +0530

    routes added done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux'
+
 import './App.css'
-import { RootState } from './redux/store'
-import { decrement, increment } from './redux/features/counter/counterSlice';
+import MainLayout from './components/layouts/MainLayout';
+
+
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  
   
 
   return (
     <>
-      <div className='m-20'>
-        <h1>Value : {count}</h1>
-        <button onClick={() => dispatch(increment())} className='bg-blue-800 p-2 text-white font-bold rounded-md mt-2'>Add</button>
-        <button onClick={() => dispatch(decrement())} className='bg-blue-800 p-2 text-white font-bold ml-4 rounded-md mt-2'>Subtract</button>
-      </div>
+      <div className=' min-h-screen w-full '>
+           <MainLayout />
+    </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}>
+      <App />
+      </RouterProvider>
+    
     </Provider>
   </React.StrictMode>,
 )
