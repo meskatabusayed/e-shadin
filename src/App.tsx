c45177b30bb38ab3ee3efe87298bc5e07c16fbe1
meskatabusayed <meskatabusayed@gmail.com>
2024-08-15 15:30:03 +0530

    redux setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { RootState } from './redux/store'
+import { decrement, increment } from './redux/features/counter/counterSlice';
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   
 
   return (
     <>
       <div className='m-20'>
-        <h1>Value : 0</h1>
-        <button className='bg-blue-800 p-2 text-white font-bold rounded-md mt-2'>Add</button>
-        <button className='bg-blue-800 p-2 text-white font-bold ml-4 rounded-md mt-2'>Subtract</button>
+        <h1>Value : {count}</h1>
+        <button onClick={() => dispatch(increment())} className='bg-blue-800 p-2 text-white font-bold rounded-md mt-2'>Add</button>
+        <button onClick={() => dispatch(decrement())} className='bg-blue-800 p-2 text-white font-bold ml-4 rounded-md mt-2'>Subtract</button>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterReducer from './features/counter/counterSlice'
 export const store = configureStore({
-    reducer : {}
+    reducer : {
+        counter : counterReducer,
+    }
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
