19118b832a901b2ccd04df436e251c81c11abb71
meskatabusayed <meskatabusayed@gmail.com>
2024-08-16 22:03:31 +0530

    cart created done

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,11 +1,25 @@
 
 
 const Cart = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  )
-}
+//   const products = getAllProducts();
 
-export default Cart
+  
+  return (
+    <div className="container mt-10 mx-auto">
+      <div className="flex lg:flex-row flex-col-reverse justify-center lg:space-x-40 ">
+        {/* <div className="space-y-5 lg:mt-0 mt-5">
+          {products.length ? (
+            products.map((product: any) => (
+              <CartDetails key={product.id} product={product} />
+            ))
+          ) : (
+            <p className="text-2xl text-red-500"> not product found</p>
+          )}
+        </div> */}
+        <OrderSummary />
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
