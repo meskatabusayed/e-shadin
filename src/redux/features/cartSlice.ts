0e79ab02aa184870cd4c5ba835df8e2d6d4c1bbe
meskatabusayed <meskatabusayed@gmail.com>
2024-08-16 23:28:21 +0530

    cart slice create

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -3,17 +3,30 @@ import {createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    value : 0
+    products : [] as any,
+    selectedItems : 0,
+    totalPrice : 0,
+    tax : 0,
+    taxRate : 0.1,
+    grandTotal : 0
 }
 
 export const cartSlice = createSlice({
     name : 'cart',
     initialState,
     reducers: {
+        addToCart : (state , action) => {
+            const isExist = state.products.find(product => product.id === action.payload.id)
+            if(!isExist){
+                state.products.push({...action.payload , quantity : 1})
+
+            }
+
+        }
 
     }
 
 });
 
-
+export const { } = cartSlice.actions;
 export default cartSlice.reducer;
