03fd585c444f51cb4d4aef34fe9144e2c2bf2b59
meskatabusayed <meskatabusayed@gmail.com>
2024-08-17 00:43:40 +0530

    cartSlice update

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {createSlice } from "@reduxjs/toolkit";
 
 
@@ -18,15 +19,24 @@ export const cartSlice = createSlice({
         addToCart : (state , action) => {
             const isExist = state.products.find(product => product.id === action.payload.id)
             if(!isExist){
-                state.products.push({...action.payload , quantity : 1})
+                state.products.push({...action.payload , quantity : 1 , })
 
             }
+            state.selectedItems = state.products.reduce((total : number , product : any) => {
+                return Number(total + product.quantity);
+            } , 0)
 
         }
 
     }
 
 });
+
+// export const setSelectedItems = (state : any) => {
+//     state.products.reduce((total : number , product : any) => {
+//         return Number(total + product.quantity);
+//     } , 0)
+// }
 
 export const {addToCart} = cartSlice.actions;
 export default cartSlice.reducer;
